2ff6e42fc40b3a5dc67109cb43a7537aaf3666d6
agusjuli <agusjuli@yahoo.com.sg>
2025-04-06 17:13:49 +0800

    Adding new s3 bucket_r3

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.3.0"  # Added to satisfy terraform_required_version rule
+  required_version = ">= 1.3.0" # Added to satisfy terraform_required_version rule
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"        # Added to satisfy terraform_required_providers rule
+      version = "~> 5.0" # Added to satisfy terraform_required_providers rule
     }
   }
 
@@ -21,10 +21,10 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]  # Removed deprecated interpolation
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1] # Removed deprecated interpolation
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"  # This is acceptable string concatenation
+  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}" # This is acceptable string concatenation
 }
